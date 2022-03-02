6dba8fa6243f44909658f551113a0bc605ae0638
bluepill5 <alexandro.mayoral@gmail.com>
2022-03-01 19:50:08 -0600

    Triggers update

--- scripts/triggersDB.sql
+++ scripts/triggersDB.sql
@@ -26,6 +26,9 @@ CREATE TABLE AUDITORIA(
 /* -------------------- Trigger cuando se tiene un update ------------------- */
 /* --- Se guarda en la tabla de auditoria el tipo de movimeinto y usuario --- */
 
+-- TRIGGER TRG_INSTITUCIONES: Trigger que sirvve para bitacorear cuando se actualizan
+-- los datosm almacenando en la tabla de auditoría los folios y el usuario que realizó
+-- la actualización
 DELIMITER //
 DROP TRIGGER IF EXISTS TRG_INSTITUCIONES//
 CREATE TRIGGER TRG_INSTITUCIONES
@@ -37,6 +40,7 @@ BEGIN
 END//
 DELIMITER ;
 
+-- Ejemplo:
 UPDATE CREDITO
 SET SALDO = 555, MONTO_EXIGIBLE = 0
 WHERE INSTITUCION='40012' AND FOLIO = 'ABC1';
@@ -44,8 +48,10 @@ WHERE INSTITUCION='40012' AND FOLIO = 'ABC1';
 /* ------------------- Trigger cuando se tiene un insert ------------------- */
 /* -- Se controla el saldo que se ingresa antes de que se inserte el valor -- */
 
+-- TRG_CREDITOS_BEFORE: Trigger que en cada inserción de los datos si el valor del
+-- saldo es mayor o menor a un umbral, lo topa a dicho umbral
 DELIMITER //
-DROP TRIGGER IF EXISTS TRG_CREDITOS_BEGORE//
+DROP TRIGGER IF EXISTS TRG_CREDITOS_BEFORE//
 CREATE TRIGGER TRG_CREDITOS_BEFORE
 BEFORE INSERT ON CREDITO
 FOR EACH ROW
@@ -58,13 +64,12 @@ BEGIN
 END//
 DELIMITER ;
 
-INSERT INTO CREDITO(INSTITUCION, FOLIO, ID_PRODUCTO, FOLIO_CLIENTE, SALDO, ATRASOS, MONTO_EXIGIBLE, PAGO_REALIZADO, FECHA_INICIO)
-VALUES
-    ('40012', 'NOM1', 'PROD1', 'CLIE1', 99999999, 0, 100, 50, '2022/01/17'),
-	('40012', 'NOM2', 'PROD2', 'CLIE1', -99999, 0, 100, 50, '2022/01/17');
 
 /* ----------------------- Trigger después del insert ----------------------- */
 /* --- Se guarda en la tabla de auditoria el tipo de movimeinto y usuario --- */
+
+-- TRG_CREDITOS_AFTER: Trigger que bitacorea las inserciones a las tablas guardando 
+-- en la tabla de auditoría el usuario y folio integrado
 DELIMITER //
 DROP TRIGGER IF EXISTS TRG_CREDITOS_AFTER//
 CREATE TRIGGER TRG_CREDITOS_AFTER
@@ -75,6 +80,12 @@ BEGIN
     VALUES ('INSERT', '', NEW.FOLIO, CURRENT_USER);
 END//
 DELIMITER ;
+
+-- Ejemplo:
+INSERT INTO CREDITO(INSTITUCION, FOLIO, ID_PRODUCTO, FOLIO_CLIENTE, SALDO, ATRASOS, MONTO_EXIGIBLE, PAGO_REALIZADO, FECHA_INICIO)
+VALUES
+    ('40012', 'NOM1', 'PROD1', 'CLIE1', 99999999, 0, 100, 50, '2022/01/17'),
+	('40012', 'NOM2', 'PROD2', 'CLIE1', -99999, 0, 100, 50, '2022/01/17');
 
 
 
